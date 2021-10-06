7f2d7fb0efff48665a665b8ceb984fba385d9f53
abroroo <abror_iskandarov@yahoo.com>
2021-10-06 15:29:26 +0900

    Login button.js added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 
 
-ReactDOM.render(<App 
+
+ReactDOM.render(
+<Router>
+  <Auth0ProviderWithHistory>
+  <App 
   githubURL="https://github.com/completejavascript/pomodoro-clock" 
   defaultBreakLength='5' 
-  defaultSessionLength='25' />, 
+  defaultSessionLength='25' />
+  </Auth0ProviderWithHistory>
+</Router>, 
   document.getElementById('root')
 );
 
